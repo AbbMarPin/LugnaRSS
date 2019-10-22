c6b3dcf10b243a2ff6866df487c09d5e6fbea126
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-10-22 10:11:21 +0200

    now with more speetch

--- Mat.py
+++ Mat.py
@@ -1,6 +1,10 @@
 import html
 import feedparser
 import datetime
+from gtts import gTTS
+import os
+import playsound
+from time import sleep
 
 d = feedparser.parse("https://www.fazerfoodco.se/modules/MenuRss/MenuRss/CurrentWeek?costNumber=6417&language=sv")
 
@@ -14,3 +18,7 @@ for x in range(5):
 
 for x in foodlist[datetime.datetime.today().weekday()]:
     print(x)
+    tts = gTTS(text=x, lang='sv')
+    tts.save("say.mp3")
+    playsound.playsound('say.mp3', True)
+    os.remove("say.mp3")

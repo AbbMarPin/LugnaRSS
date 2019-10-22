288ec8c54b071c78580f6cc0761c70acb9d05d9a
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-10-22 09:02:10 +0200

    Update Mat.py

--- Mat.py
+++ Mat.py
@@ -1,12 +1,16 @@
 import html
 import feedparser
+import datetime
 
 d = feedparser.parse("https://www.fazerfoodco.se/modules/MenuRss/MenuRss/CurrentWeek?costNumber=6417&language=sv")
 
+foodlist = []
 
-food = html.unescape(d.entries[0].description)
-food = food.replace("<p>","").replace("</p>","")
+for x in range(5):
+    food = html.unescape(d.entries[x].description)
+    food = food.replace("<p>","").replace("</p>","")
+    food = food.replace("<br />","")
+    foodlist.append(food.split("\n"))
 
-food = food.split("\n")
-
-print(food)
+for x in foodlist[datetime.datetime.today().weekday()]:
+    print(x)

1dc6d12e6ba432e72251e95301de4ca20695c513
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-10-22 12:12:41 +0200

    exit fix

--- Mat.py
+++ Mat.py
@@ -15,10 +15,15 @@ for x in range(5):
     food = food.replace("<p>","").replace("</p>","")
     food = food.replace("<br />","")
     foodlist.append(food.split("\n"))
-
-for x in foodlist[datetime.datetime.today().weekday()]:
-    print(x)
-    tts = gTTS(text=x, lang='sv')
-    tts.save("say.mp3")
-    playsound.playsound('say.mp3', True)
-    os.remove("say.mp3")
+try:
+    for x in foodlist[datetime.datetime.today().weekday()]:
+        print(x)
+        tts = gTTS(text=x, lang='sv')
+        tts.save("say.mp3")
+        playsound.playsound('say.mp3', True)
+        os.remove("say.mp3")
+except:
+    try:
+        os.remove("say.mp3")
+    except:
+        print("exiting")
